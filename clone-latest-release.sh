4b31fdc7bdc641cc30d17bf6348b33876866c79d
Anhad Jai Singh <ajaisingh@tower-research.com>
2018-01-10 20:51:37 +0530

    Change version of pdns to clone

--- clone-latest-release.sh
+++ clone-latest-release.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 # We use HTTPS because we can clone unauth'd
 GIT_REPO='https://github.com/PowerDNS/pdns.git'
 
 git clone ${GIT_REPO}
 
 pushd pdns
-LATEST_RELEASE_TAG="$(git tag --list auth-* --sort='version:refname' | grep -E -v 'rc|alpha|beta' | tail -1)"
-git checkout ${LATEST_RELEASE_TAG}
+# The current release doesn't have the swagger-spec in it, so let's use master for now
+#LATEST_RELEASE_TAG="$(git tag --list auth-* --sort='version:refname' | grep -E -v 'rc|alpha|beta' | tail -1)"
+#git checkout ${LATEST_RELEASE_TAG}
 popd
