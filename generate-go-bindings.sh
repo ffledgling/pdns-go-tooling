8a79ea7ff1e76da1d885b2467ba0144e912da2fa
Anhad Jai Singh <ajaisingh@tower-research.com>
2018-01-10 18:52:24 +0530

    Fix silly mistake in binding-gen

--- generate-go-bindings.sh
+++ generate-go-bindings.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate \
-    -c config.json
-    -i local/pdns/docs/http-api/swagger/authoritative-api-swagger.yaml \
+set -euxo pipefail
+
+sudo docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli generate \
+    -c /local/swagger-config.json \
+    -i /local/pdns/docs/http-api/swagger/authoritative-api-swagger.yaml \
     -l go \
     -o /local/out/go
